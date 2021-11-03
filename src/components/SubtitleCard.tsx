bd2b69a7ecf7f61c64a3246b575f2cba3ce085e0
Breno Pinheiro <davepinh@gmail.com>
2021-11-03 13:38:35 -0300

    Bugfix: subtitle order label

--- src/components/SubtitleCard.tsx
+++ src/components/SubtitleCard.tsx
@@ -6,20 +6,20 @@ import Text from "../types/text"
 import "./styles/SubtitleCard.css"
 interface SubtitleCardProps {
   id: number
-  key?: string
+  subKey: string
   crop: Crop
 }
 
 const SubtitleCard: React.FC<SubtitleCardProps> = props => {
-  const { key, crop, id } = props
+  const { subKey, crop, id } = props
 
   const [isTextareaVisible, setTextareaVisible] = useState(false)
 
   const { dispatch } = useContext(StoreContext)
 
   return (
-    <div key={key} className="card" style={{ margin: "1rem" }}>
-      <div style={{ marginBottom: "0.4rem" }}><span className="badge">{key}</span></div>
+    <div key={subKey} className="card" style={{ margin: "1rem" }}>
+      <div style={{ marginBottom: "0.4rem" }}><span className="badge">{subKey}</span></div>
       <div><span>{crop.start}</span> ➡️ <span>{crop.end}</span></div>
       <div>
         {crop.texts.map((text, i) => {
@@ -34,11 +34,8 @@ const SubtitleCard: React.FC<SubtitleCardProps> = props => {
             onBlur={(e) => {
               const t: Text = { order: crop.texts.length, value: e.target.value }
               crop.texts = [...crop.texts, t]
-              // TODO: test
               dispatch({ type: ActionType.REPLACE_CROP, payload: {index: id, crop: crop} })
 
-              // state.crops[id] = crop
-              // setCrops(crops)
               setTextareaVisible(false)
             }} />
         </div>}
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -10,29 +10,33 @@ const Editor: React.FC = () => {
   const { state } = useContext(StoreContext)
 
   return (
-    <div className="container">
-      <div style={{ width: "10%" }}>
-        <Timeline
-          width={100}
-          height={window.innerHeight}
-          minimumScale={10}
-          minimumScaleTime={1}
-          minimumScalesInLongScale={10}
-          lineWidth={1}
-          offsetLeft={0}
-          lineColor="#666"
-          longLineColor="#000"
-        />
+    <>
+      <div className="container">
+        <div style={{ width: "10%" }}>
+          <Timeline
+            width={100}
+            height={window.innerHeight}
+            minimumScale={10}
+            minimumScaleTime={1}
+            minimumScalesInLongScale={10}
+            lineWidth={1}
+            offsetLeft={0}
+            lineColor="#666"
+            longLineColor="#000"
+          />
+        </div>
+        <div style={{ width: "40%", padding: "1rem", height: "100%", overflowY: "auto" }}>
+          {
+            state.crops.map((crop: Crop, i: number) =>
+              <SubtitleCard id={i} subKey={(i + 1).toString()} crop={crop} />
+            )
+          }
+        </div>
+        <div style={{ width: "50%" }}>
+          <Video src="/BigBuckBunny.mp4" width="640" height="480" controls={true} />
+        </div>
       </div>
-      <div style={{ width: "40%", padding: "1rem", height: "100%", overflowY: "auto" }}>
-        {state.crops.map((crop: Crop, i: number) =>
-          <SubtitleCard id={i} key={i.toString()} crop={crop} />
-        )}
-      </div>
-      <div style={{ width: "50%" }}>
-        <Video src="/BigBuckBunny.mp4" width="640" height="480" controls={true} />
-      </div>
-    </div>
+    </>
   )
 }
 
