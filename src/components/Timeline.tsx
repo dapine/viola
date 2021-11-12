d9717f0b6e2da5b15a2eb14902ccdb827d31a1f0
Breno Pinheiro <davepinh@gmail.com>
2021-11-12 11:19:44 -0300

    Timeline component draw functions moved to a separate file

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,8 +1,8 @@
-import { useCallback, useContext, useEffect, useRef, useState } from "react"
+import { MouseEvent, useCallback, useContext, useEffect, useRef, useState, WheelEvent } from "react"
 import { ActionType } from "../store/actionTypes"
 import { StoreContext } from "../store/StoreContext"
 import Crop from "../types/crop"
-import { formatSeconds } from "../utils/utils"
+import { drawCropList, drawLineTimelinePosition, drawTimeline } from "../utils/draw"
 
 export interface TimelineProps {
   width: number
@@ -16,27 +16,6 @@ export interface TimelineProps {
   longLineColor: string
 }
 
-const drawLine = (ctx: any, beginX: number, beginY: number, endX: number,
-  endY: number, lineColor: string, lineWidth: number) => {
-  ctx.beginPath()
-  ctx.moveTo(beginX, beginY)
-  ctx.lineTo(endX, endY)
-  ctx.strokeStyle = lineColor
-  ctx.lineWidth = lineWidth
-  ctx.stroke()
-}
-
-const drawSolidRect = (ctx: any, beginX: number, beginY: number, endX: number,
-  endY: number, color: string) => {
-  ctx.beginPath()
-  for (let i = beginX; i < endX; i++) {
-    ctx.moveTo(i, beginY)
-    ctx.lineTo(i, endY)
-  }
-  ctx.strokeStyle = color
-  ctx.stroke()
-}
-
 const Timeline: React.FC<TimelineProps> = props => {
   const ref = useRef(null)
   const [mouseY, setMouseY] = useState(0.0)
@@ -46,44 +25,81 @@ const Timeline: React.FC<TimelineProps> = props => {
   const { width, height, minimumScale, minimumScalesInLongScale,
     minimumScaleTime, offsetLeft, lineColor, longLineColor, lineWidth } = props
 
-  const drawMousePosition = useCallback((ctx: any) => {
-    drawLine(ctx, 0, mouseY, 20, mouseY, "#ff0000", 2)
-  }, [mouseY])
+  const handleWheel = (e: WheelEvent<HTMLCanvasElement>) => {
+    e.preventDefault()
+    const cur = minimumScale + (e.deltaY * 0.01) * -1
+    const newHeight = height + (e.deltaY * 7) * -1
+    dispatch({ type: ActionType.SET_TIMELINE_MINIMUM_SCALE, payload: cur })
+    dispatch({ type: ActionType.SET_TIMELINE_HEIGHT, payload: newHeight })
+  }
 
-  const drawCrops = useCallback((ctx: any) => {
-    state.crops.forEach((crop: Crop) => {
-      if (crop.start && crop.end) {
-        const s = (crop.start * minimumScale) / minimumScaleTime
-        const e = (crop.end * minimumScale) / minimumScaleTime
+  const handleClick = () => {
+    const time = mouseY / minimumScale * minimumScaleTime
 
-        const color = crop.selected ? "#B85C57" : "#98CE8F"
+    dispatch({ type: ActionType.DESELECT_ALL_CROPS })
 
-        drawSolidRect(ctx, 0, s, 15, e, color)
-      }
-    })
-  }, [state.crops, minimumScale, minimumScaleTime])
+    // "Incomplete crop" means a crop with start time only
+    const hasIncompleteCrop = state.crops.some((crop: Crop) => !('end' in crop))
 
-  const draw = useCallback((ctx: any) => {
-    ctx.clearRect(0, 0, width, height)
+    // XXX: rewrite this in a more functional/immutable way
+    if (hasIncompleteCrop) {
+      const index = state.crops.findIndex((crop: Crop) => !('end' in crop))
+      let crop: Crop = state.crops[index]
 
-    ctx.fillStyle = "#999"
-    ctx.font = "12px Arial"
-
-    const beginX = 0
-    const endX = 20
-
-    for (let i = 0; i < height; i += minimumScale) {
-      if (i % (minimumScale * minimumScalesInLongScale) === 0) {
-        drawLine(ctx, beginX, i + offsetLeft, endX, i + offsetLeft, longLineColor, lineWidth)
-
-        const showTime = (i / minimumScale) * minimumScaleTime
-        ctx.fillText(formatSeconds(showTime), endX, i + offsetLeft)
+      if (time > crop.start) {
+        crop.end = time
       } else {
-        drawLine(ctx, beginX, i + offsetLeft, 10, i + offsetLeft, lineColor, lineWidth)
+        crop.end = crop.start
+        crop.start = time
+      }
+
+      dispatch({ type: ActionType.REPLACE_CROP, payload: { index: index, crop: crop } })
+    } else {
+      const selected = state.crops.find((crop: Crop) => {
+        return crop.start <= time && crop.end! >= time
+      })
+
+      if (selected) {
+        const index = state.crops.findIndex((crop: Crop) => crop === selected)
+
+        dispatch({ type: ActionType.SELECT_CROP, payload: index })
+      } else {
+        let crop: Crop = { start: time, texts: [], selected: false }
+        dispatch({ type: ActionType.ADD_CROP, payload: { crop: crop } })
+        const sorted = state.crops.sort((a: Crop, b: Crop) => {
+          return a.start - b.start
+        })
+        dispatch({ type: ActionType.REPLACE_ALL_CROPS, payload: { crops: sorted } })
       }
     }
-  }, [width, height, minimumScale, minimumScalesInLongScale, offsetLeft, lineColor,
-    lineWidth, longLineColor, minimumScaleTime])
+
+    dispatch({ type: ActionType.SET_CURRENT_VIDEO_TIME, payload: time })
+  }
+
+  const handleMouseMove = (e: MouseEvent) => {
+    // @ts-ignore: Object is possibly 'null'.
+    const rect = ref.current.getBoundingClientRect()
+    let y = e.clientY - rect.top
+
+    setMouseY(y)
+
+    if (e.ctrlKey) {
+      const time = mouseY / minimumScale * minimumScaleTime
+      dispatch({ type: ActionType.SET_CURRENT_VIDEO_TIME, payload: time })
+    }
+  }
+
+  const drawPosition = useCallback((ctx: any) => drawLineTimelinePosition(ctx, mouseY), [mouseY])
+
+  const drawCrops = useCallback((ctx: any) =>
+    drawCropList(ctx, state.crops, minimumScale, minimumScaleTime),
+    [state.crops, minimumScale, minimumScaleTime])
+
+  const draw = useCallback((ctx: any) =>
+    drawTimeline(ctx, width, height, minimumScale, minimumScaleTime, minimumScalesInLongScale,
+      offsetLeft, lineColor, longLineColor, lineWidth),
+    [width, height, minimumScale, minimumScalesInLongScale, offsetLeft, lineColor,
+      lineWidth, longLineColor, minimumScaleTime])
 
   useEffect(() => {
     const canvas = ref?.current
@@ -95,7 +111,7 @@ const Timeline: React.FC<TimelineProps> = props => {
     const render = () => {
       draw(ctx)
       drawCrops(ctx)
-      drawMousePosition(ctx)
+      drawPosition(ctx)
       animationFrame = window.requestAnimationFrame(render)
     }
     render()
@@ -104,70 +120,12 @@ const Timeline: React.FC<TimelineProps> = props => {
       window.cancelAnimationFrame(animationFrame)
     }
 
-  }, [mouseY, drawCrops, draw, drawMousePosition])
+  }, [mouseY, drawCrops, draw, drawPosition])
 
   return <canvas ref={ref} {...props}
-    onMouseMove={(e) => {
-      // @ts-ignore: Object is possibly 'null'.
-      const rect = ref.current.getBoundingClientRect()
-      let y = e.clientY - rect.top
-
-      setMouseY(y)
-
-      if (e.ctrlKey) {
-        const time = mouseY / minimumScale * minimumScaleTime
-        dispatch({ type: ActionType.SET_CURRENT_VIDEO_TIME, payload: time })
-      }
-    }}
-    onWheel={(e) => {
-      e.preventDefault()
-      const cur = minimumScale + (e.deltaY * 0.01) * -1
-      const newHeight = height + (e.deltaY * 7) * -1
-      dispatch({ type: ActionType.SET_TIMELINE_MINIMUM_SCALE, payload: cur })
-      dispatch({ type: ActionType.SET_TIMELINE_HEIGHT, payload: newHeight })
-    }}
-    onClick={() => {
-      const time = mouseY / minimumScale * minimumScaleTime
-
-      dispatch({ type: ActionType.DESELECT_ALL_CROPS })
-
-      // "Incomplete crop" means a crop with start time only
-      const hasIncompleteCrop = state.crops.some((crop: Crop) => !('end' in crop))
-
-      // XXX: rewrite this in a more functional/immutable way
-      if (hasIncompleteCrop) {
-        const index = state.crops.findIndex((crop: Crop) => !('end' in crop))
-        let crop: Crop = state.crops[index]
-
-        if (time > crop.start) {
-          crop.end = time
-        } else {
-          crop.end = crop.start
-          crop.start = time
-        }
-
-        dispatch({ type: ActionType.REPLACE_CROP, payload: { index: index, crop: crop } })
-      } else {
-        const selected = state.crops.find((crop: Crop) => {
-          return crop.start <= time && crop.end! >= time
-        })
-
-        if (selected) {
-          const index = state.crops.findIndex((crop: Crop) => crop === selected)
-
-          dispatch({ type: ActionType.SELECT_CROP, payload: index })
-        } else {
-          let crop: Crop = { start: time, texts: [], selected: false }
-          dispatch({ type: ActionType.ADD_CROP, payload: { crop: crop } })
-          const sorted = state.crops.sort((a: Crop, b: Crop) => {
-            return a.start - b.start
-          })
-          dispatch({ type: ActionType.REPLACE_ALL_CROPS, payload: { crops: sorted } })
-        }
-      }
-
-      dispatch({ type: ActionType.SET_CURRENT_VIDEO_TIME, payload: time })
-    }}
+    onMouseMove={handleMouseMove}
+    onWheel={handleWheel}
+    onClick={handleClick}
   />
 }
 
---
+++ src/utils/draw.ts
@@ -0,0 +1,63 @@
+import Crop from "../types/crop"
+import { formatSeconds } from "./utils"
+
+export const drawLine = (ctx: any, beginX: number, beginY: number, endX: number,
+  endY: number, lineColor: string, lineWidth: number) => {
+  ctx.beginPath()
+  ctx.moveTo(beginX, beginY)
+  ctx.lineTo(endX, endY)
+  ctx.strokeStyle = lineColor
+  ctx.lineWidth = lineWidth
+  ctx.stroke()
+}
+
+export const drawSolidRect = (ctx: any, beginX: number, beginY: number, endX: number,
+  endY: number, color: string) => {
+  ctx.beginPath()
+  for (let i = beginX; i < endX; i++) {
+    ctx.moveTo(i, beginY)
+    ctx.lineTo(i, endY)
+  }
+  ctx.strokeStyle = color
+  ctx.stroke()
+}
+
+export const drawLineTimelinePosition = (ctx: any, mouseY: number,
+  beginX = 0, endX = 20, lineColor = "#ff0000", lineWidth = 2) => {
+  drawLine(ctx, beginX, mouseY, endX, mouseY, lineColor, lineWidth)
+}
+
+export const drawCropList = (ctx: any, crops: Array<Crop>, minimumScale: number, minimumScaleTime: number,
+  colorCrop = "#98CE8F", colorCropSelected = "#B85C57", beginX = 0, endX = 15) => {
+  crops.forEach((crop: Crop) => {
+    if (crop.start && crop.end) {
+      const s = (crop.start * minimumScale) / minimumScaleTime
+      const e = (crop.end * minimumScale) / minimumScaleTime
+
+      const color = crop.selected ? colorCropSelected : colorCrop
+
+      drawSolidRect(ctx, beginX, s, endX, e, color)
+    }
+  })
+}
+
+export const drawTimeline = (ctx: any, width: number, height: number, minimumScale: number,
+  minimumScaleTime: number, minimumScalesInLongScale: number, offsetLeft: number, 
+  lineColor: string, longLineColor: string, lineWidth: number, beginX = 0, endX = 20,
+  fillStyle = "#999", font = "12px sans-serif") => {
+  ctx.clearRect(0, 0, width, height)
+
+  ctx.fillStyle = fillStyle
+  ctx.font = font
+
+  for (let i = 0; i < height; i += minimumScale) {
+    if (i % (minimumScale * minimumScalesInLongScale) === 0) {
+      drawLine(ctx, beginX, i + offsetLeft, endX, i + offsetLeft, longLineColor, lineWidth)
+
+      const showTime = (i / minimumScale) * minimumScaleTime
+      ctx.fillText(formatSeconds(showTime), endX, i + offsetLeft)
+    } else {
+      drawLine(ctx, beginX, i + offsetLeft, 10, i + offsetLeft, lineColor, lineWidth)
+    }
+  }
+}
